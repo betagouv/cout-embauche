e1d4a4b012f0a43e6a9b876ce8349eb2cd58f171
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-12 18:57:07 +0100

    Display detailed results in simulation page

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 function display(data) {
 	Object.keys(data).forEach(function(toSet) {
-		var target = document.querySelector('#result [data-source="' + toSet + '"]'),
+		var target = document.querySelector('[data-source="' + toSet + '"]'),
 			value  = data[toSet];
 
 		if (! target)

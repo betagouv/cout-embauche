73789b851f478b54761f07ec8e9e09dfbdeaf1c7
Matti Schneider <matti.schneider@data.gouv.fr>
2015-07-10 10:38:12 +0200

    Improve rendering performance

--- entry.js
+++ entry.js
@@ -1,9 +1,9 @@
 document.write(require('./assets/embauche.html'));
 
-require('./js/index');
-
 require('./css/main.css');
 require('./css/details.css');
 require('./css/tooltip.css');
+
+require('./js/index');
 
 require('./lib/details.polyfill.js');

71fa5d59b92c09e1f7132e8bcd911bc5cafc6f24
Matti Schneider <matti.schneider@data.gouv.fr>
2015-08-04 16:58:09 +0200

    Compile Bootstrap compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
+var webpack = require('webpack');
+
+
 module.exports = {
-    entry: './widget.js',
+    entry: {
+        'cout-embauche-widget': './widget.js',
+        'bootstrap-compat': './js/compat/bootstrap.js',
+    },
     output: {
-        path: __dirname + '/dist',
-        filename: 'cout-embauche-widget.js'
+        path: 'dist',
+        filename: '[name].js',
     },
     module: {
         loaders: [
             { test: /\.css$/,  loader: 'style!css' },
             { test: /\.html$/, loader: 'html' },
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'cout-embauche-widget',
+            filename: 'cout-embauche-widget.js',
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'bootstrap-compat',
+            filename: 'bootstrap-compat.js',
+            async: true,
+        }),
+    ]
 };

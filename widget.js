0b7400a7fc15d106923a78e24353eb1872a622e5
Matti Schneider <matti.schneider@data.gouv.fr>
2015-08-04 17:56:13 +0200

    Avoid FoUC in widget

--- widget.js
+++ widget.js
@@ -1,8 +1,8 @@
-document.write(require('./assets/embauche.html'));
-
 require('./css/main.css');
 require('./css/details.css');
 require('./css/tooltip.css');
+
+document.write(require('./assets/embauche.html'));
 
 window.Embauche = require('./js/index');
 

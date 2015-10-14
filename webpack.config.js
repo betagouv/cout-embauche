2ed1f5670967bb5f07ee55f761b1f6d0feb2dc63
Matti Schneider <matti.schneider@data.gouv.fr>
2015-10-14 10:21:23 +0200

    Respect eslint in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,24 +12,29 @@ module.exports = {
         filename: '[name].js',
     },
     module: {
-        loaders: [
-            { test: /\.css$/,  loader: 'style!css!postcss-loader' },
-            { test: /\.html$/, loader: 'html' },
-        ],
-        preLoaders: [
-            {test: /\.js$/, loader: "eslint-loader", exclude: /(node_modules|lib)/}
-        ]
+        loaders: [ {
+            test: /\.css$/,
+            loader: 'style!css!postcss-loader',
+        }, {
+            test: /\.html$/,
+            loader: 'html',
+        } ],
+        preLoaders: [ {
+            test: /\.js$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules|lib)/,
+        } ],
     },
     eslint: {
-       configFile: './.eslintrc'
+        configFile: './.eslintrc',
     },
     postcss: function () {
         return [
           prefix({
             prefix: '.SGMAPembauche ', // <--- notice the trailing space!
             exclude: [ '.SGMAPembauche' ], // to style the prefix container itself
-          })
-        ];
+          }),
+        ]
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
@@ -41,5 +46,5 @@ module.exports = {
             filename: 'bootstrap-compat.js',
             async: true,
         }),
-    ]
-};
+    ],
+}

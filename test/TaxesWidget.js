ac3bbcb8ca6844671c590d8b1b8885bdaaa00e54
Mael <mael.thomas@data.gouv.fr>
2015-10-26 11:48:48 +0100

    Add imprecise line tooltip test
    Hover over the value, check if something is displayed in the tooltip

--- test/TaxesWidget.js
+++ test/TaxesWidget.js
@@ -2,4 +2,12 @@ root				: '#taxes',
 toggleLink			: '#taxes summary',
 taxesList			: '#taxes .category',
 randomTax			: '#taxes [data-source]',
+impreciseTaxValue  : '#taxes .imprecise [data-source]',
+impreciseTaxTooltipText  : '#taxes .imprecise .explanation div',
 createTestButton	: '#createTest',
+
+hoverOverImpreciseTax: function() {
+    return this.impreciseTaxValue.then(function(resultElement) {
+        return resultElement.moveTo()
+    })
+}

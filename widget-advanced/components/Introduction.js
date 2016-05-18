cf2820029b7b8474794729bb6d334330af980028
Mael Thomas <mael.thomas@data.gouv.fr>
2016-05-18 11:26:24 +0200

    Simplify advaced sim introduction text

--- widget-advanced/components/Introduction.js
+++ widget-advanced/components/Introduction.js
@@ -5,7 +5,7 @@ export default () =>
   <div id="conversation-introduction">
     <img src={hi} />
     <p>
-      Des questions vont apparaître, répondez-y et les résultats de simulation se mettront à jour.
+      Répondez aux questions suivantes, et les résultats de simulation se préciseront.
     </p>
     <p>
       Revenez sur vos pas en cliquant sur vos réponses.

ada56bb744660481051ef46d1bffa2dce22de4cd
Mael Thomas <mael.thomas@data.gouv.fr>
2016-11-25 11:45:19 +0100

    Update circleci conf
    Use reasonable test timeouts, to account for test machine network latency
    
    Dropping full multi browser testing (breaks too often)
    Using headless dom testing now

--- source/test/test.js
+++ source/test/test.js
@@ -28,7 +28,7 @@ let nodeToNumber = (node) =>
 	+node.text().replace('€', '').replace(',', '')
 
 describe('Test du simulateur de coût d\'embauche', function() {
-	this.timeout(5000)
+	this.timeout(10 * 1000)
 
 	let app = mount(<App store={store} />)
 
@@ -36,14 +36,14 @@ describe('Test du simulateur de coût d\'embauche', function() {
 		setTimeout(() => {
 			expect(nodeToNumber(app.find('.figure').first())).to.be.above(3000)
 			done()
-		}, 1000)
+		}, 5000)
   })
 	it('Ce salaire doit grossir après une élevation du salaire brut', (done) => {
 		app.find('#salaire').simulate('change', {target: {value: '6000'}})
 		setTimeout(() => {
 			expect(nodeToNumber(app.find('.figure').first())).to.be.above(8000)
 			done()
-		}, 1000)
+		}, 2000)
 	})
 	it('Les détails doivent pouvoir s\'afficher', (done) => {
 		app.find('.show-details').simulate('click')
@@ -69,7 +69,7 @@ describe('Test du simulateur de coût d\'embauche', function() {
 		setTimeout(() => {
 			expect(nodeToNumber(app.find('.figure').first())).to.be.above(50000) // une mutuelle onéreuse :o
 			done()
-		}, 1000)
+		}, 2000)
 	})
 
 	it('Voir une nouvelle question après avoir validé la première<', (done) => {
@@ -80,19 +80,4 @@ describe('Test du simulateur de coût d\'embauche', function() {
 		done()
 	})
 
-  //   ;
-  //   expect(wrapper.props().bar).to.equal("baz");
-  //   wrapper.setProps({ bar: "foo" });
-  //   expect(wrapper.props().bar).to.equal("foo");
-  // });
-	//
-  // it('simulates click events', () => {
-  //   const onButtonClick = sinon.spy();
-  //   const wrapper = mount(
-  //     <Foo onButtonClick={onButtonClick} />
-  //   );
-  //   wrapper.find('button').simulate('click');
-  //   expect(onButtonClick.calledOnce).to.equal(true);
-  // });
-
-});
+})

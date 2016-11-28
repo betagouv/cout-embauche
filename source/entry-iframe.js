5b1ab9a300072e32a74dfe56ea3dfb0286aa3209
Mael Thomas <mael.thomas@data.gouv.fr>
2016-11-28 14:13:28 +0100

    Correct path in entry-iframe.js

--- source/entry-iframe.js
+++ source/entry-iframe.js
@@ -4,7 +4,7 @@ let script = document.getElementById('script-simulateur-embauche'),
 	couleur = script.dataset['couleur']
 
 document.write(`
-<iframe id="SGMAPembauche" src="http://localhost:3000/iframe.html?couleur=${couleur}" width="100%" style="border: none;" scrolling="no"></iframe>
+<iframe id="SGMAPembauche" src="iframe.html?couleur=${couleur}" width="100%" style="border: none;" scrolling="no"></iframe>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.5/iframeResizer.min.js"></script>
 <script type="text/javascript">iFrameResize(null, '#SGMAPembauche')</script>
 `)

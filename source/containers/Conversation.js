95db3d2e29d066ad00ff1065f6d91b3070e5ced4
Mael Thomas <mael.thomas@data.gouv.fr>
2016-12-15 11:39:32 +0100

    Add Alsace-Moselle question

--- source/containers/Conversation.js
+++ source/containers/Conversation.js
@@ -42,7 +42,6 @@ class Conversation extends Component {
 					question="Quel est le montant total par salarié de votre complémentaire santé entreprise obligatoire ?"
 					visible={effectifEntreprise < 10 || steps.get('codeINSEE')}
 					name="mutuelle" />
-
 				<Group
 					text="Risques professionnels"
 					visible={steps.get('mutuelle')}
@@ -81,11 +80,17 @@ class Conversation extends Component {
 					name="pourcentage_alternants" />
 
 				<Question
-					title="Exonération Jeune Entreprise Innovante"
-					question="Profitez-vous du statut Jeune Entreprise Innovante pour cette embauche ?"
 					visible={
 						(effectifEntreprise < 249 && steps.get('tauxRisque'))
 						|| steps.get('pourcentage_alternants')}
+					title="Régime Alsace-Moselle"
+					question="Le salarié est-il affilié au régime d'Alsace-Moselle ?"
+					name="alsaceMoselle" />
+
+				<Question
+					title="Exonération Jeune Entreprise Innovante"
+					question="Profitez-vous du statut Jeune Entreprise Innovante pour cette embauche ?"
+					visible={steps.get('alsaceMoselle')}
 					name="jei" />
 
 				<Question

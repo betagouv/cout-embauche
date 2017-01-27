a6ffae472a4038fd21e887826aabf5d6e0ce33e2
Mael Thomas <mael.thomas@data.gouv.fr>
2017-01-27 17:49:39 +0100

    Update the link to simulateur alternance

--- source/components/InfoZone.js
+++ source/components/InfoZone.js
@@ -44,7 +44,7 @@ export default ({
 	}
 	{	infoAlternance &&
 		<span>
-			Note: pour une simulation plus fiable du cas de l'apprentissage, rendez-vous sur <a href="https://www.alternance.emploi.gouv.fr/portail_alternance/jcms/hl_5641" target="_blank">
+			Note: pour une simulation plus fiable du cas de l'apprentissage, rendez-vous sur <a href="https://www.alternance.emploi.gouv.fr/portail_alternance/jcms/hl_6238/simulateur-alternant" target="_blank">
 				le simulateur du portail de l'alternance
 			</a>
 		</span>

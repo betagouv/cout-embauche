bc52fac6237b9c5bcbcc5ab5ef307f7aa79e09d0
Mael THOMAS <mael.thomas@data.gouv.fr>
2017-05-05 15:36:04 +0200

    Move the SGMAP logo to this repo
    The previous address is not available anymore

--- source/components/Affiliation.js
+++ source/components/Affiliation.js
@@ -7,7 +7,7 @@ export default () =>
 			<img alt="OpenFisca" src="https://www.openfisca.fr/hotlinks/logo-openfisca.svg" />
 		</a>
     <a href="https://beta.gouv.fr" target="_blank">
-      <img id="logo-SGMAP" alt="Secrétariat Général pour la Modernisation de l'Action Publique" src="https://embauche.beta.gouv.fr/img/logo-SGMAP-fond-blanc.svg" />
+      <img id="logo-SGMAP" alt="Secrétariat Général pour la Modernisation de l'Action Publique" src={require('../images/logo-SGMAP.svg')} />
     </a>
 		<a id="affiliation-contact" href="mailto:contact@embauche.beta.gouv.fr?subject=A propos du simulateur d'embauche">contact</a>
   </section>
